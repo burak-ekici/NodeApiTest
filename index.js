0a58de2f721df6f3821d1c34e4531f5d484db460
Ekici-Burak <b.ekici@it-students.fr>
2022-05-28 03:07:29 +0200

    projet Node API TEST / 2

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express()
 require('./models/dbConfig')
 const postsRoutes = require('./routes/postsController')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
+app.use(cors()) // permet de donner l'acces de l'API à tout le monde , on peux par exemple utiliser notre API via le site codepen
+// app.use(cors({origin : 'http:localhost:3000'})) // permet de donner l'acces de l'API au parametre origin passé. ici seul le localhost:3000 peux envoyer les reqquetes
 
 app.use(bodyParser.json())
 
